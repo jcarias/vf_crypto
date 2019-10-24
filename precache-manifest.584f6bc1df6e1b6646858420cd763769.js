a0408b48a4d4f867cb3e57046f4aaac70acd5d61
João André Lopes Carias <carias.joao@gmail.com>
2019-10-24 23:51:49 +0100

    Updates

---
+++ precache-manifest.584f6bc1df6e1b6646858420cd763769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa35945fdeb022c7cae764f548b597c",
+    "url": "/vf_crypto/index.html"
+  },
+  {
+    "revision": "0c6b1935fd174c94b9c6",
+    "url": "/vf_crypto/static/css/main.6e7ecdf5.chunk.css"
+  },
+  {
+    "revision": "07b348ed4c37fbeb20f1",
+    "url": "/vf_crypto/static/js/2.4c627716.chunk.js"
+  },
+  {
+    "revision": "0c6b1935fd174c94b9c6",
+    "url": "/vf_crypto/static/js/main.8ba24e9f.chunk.js"
+  },
+  {
+    "revision": "6efde2f7d54b5ad167b9",
+    "url": "/vf_crypto/static/js/runtime-main.cca62f99.js"
+  }
+]);
--- src/store/rootSaga.js
+++
@@ -1,8 +0,0 @@
-import { all } from "redux-saga/effects";
-import { pollTaskWatcher } from "./dataUpdateWatcherSaga";
-
-// import watchers from other files
-export default function* rootSaga() {
-  //More sagas can be added to the array ;-)
-  yield all([pollTaskWatcher()]);
-}
